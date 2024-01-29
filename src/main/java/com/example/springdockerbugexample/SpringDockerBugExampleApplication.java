81dcf8e3f547da758616d6f93eba56fbf1f8ae47
Mishail Galtsev <galtsev.mishail@gmail.com>
2024-01-29 15:47:48 +0300

    bug in integration spring-cloud-bootstrap and spring docker compose

---
+++ src/main/java/com/example/springdockerbugexample/SpringDockerBugExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerbugexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerBugExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerBugExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdockerbugexample/SpringDockerBugExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerbugexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerBugExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
